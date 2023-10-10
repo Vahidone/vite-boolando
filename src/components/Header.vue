df6593d4aae5bd26344ab9e9536d811f7a2fbd07
Vahidone <v.ban.roma@gmail.com>
2023-10-10 21:54:26 +0200

    feat: add header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,29 +6,73 @@ export default {
 
 <template>
   <header>
-    <div class="logo">
-      <img src="/public/boolean-logo.png" alt="" />
+    <div class="container-csm">
+      <!-- MENU di sinistra del header  -->
+      <div class="menu">
+        <ul>
+          <li><a href="#">Donna</a></li>
+          <li><a href="#">Uomo</a></li>
+          <li><a href="#">Bambini</a></li>
+        </ul>
+        <!-- LOGO del header  -->
+      </div>
+      <div class="logo">
+        <img src="/public/boolean-logo.png" alt="" />
+      </div>
+
+      <!-- LOGIN del header  -->
+      <div class="login">
+        <ul>
+          <li>
+            <a href="#"><i class="fa-regular fa-user"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-regular fa-heart"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+          </li>
+        </ul>
+      </div>
     </div>
   </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use "./scss/partials/variables" as *;
 header {
   width: 100%;
   height: 70px;
-  background-color: #ff6900;
+  background-color: $header-bg-color;
   box-shadow: 0 1px 10px grey;
-  position: fixed;
-  z-index: 999;
+  .container-csm {
+    display: flex;
+    align-items: center;
+  }
 }
+
 .logo {
   width: 120px;
   height: 25px;
   margin: 25px auto;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 }
-img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+
+li {
+  display: inline-block;
+  margin-right: 10px;
+  a {
+    color: $link-color;
+    text-transform: capitalize;
+    text-decoration: none;
+    &:hover {
+      color: lighten($link-color, 70%);
+      text-decoration: underline;
+    }
+  }
 }
 </style>
